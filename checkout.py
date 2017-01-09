5f3f356091a09fa415486c89f7b90096e062a1bc
Emanuel Couto <unit73e@gmail.com>
2017-01-09 13:19:28 +0000

    Add ask password option

--- checkout.py
+++ checkout.py
@@ -1,17 +1,21 @@
 import subprocess
 import argparse
 from os.path import isdir
+from getpass import getpass
 
 SVN_COMMAND = "svn"
 SVN_DEPTH_EMPTY = "empty"
 SVN_DEPTH_INFINITY = "infinity"
 
-def _checkout(url, path, username):
+def _checkout(url, path, username, password):
     args = [SVN_COMMAND]
     args.append("co")
     if username:
         args.append("--username")
         args.append(username)
+    if password:
+        args.append("--password")
+        args.append(password)
     args.append("--depth")
     args.append(SVN_DEPTH_EMPTY)
     args.append("--quiet")
@@ -20,12 +24,15 @@ def _checkout(url, path, username):
     args.append(path)
     return subprocess.run(args, check=True)
 
-def _update(path, username):
+def _update(path, username, password):
     args = [SVN_COMMAND]
     args.append("up")
     if username:
         args.append("--username")
         args.append(username)
+    if password:
+        args.append("--password")
+        args.append(password)
     args.append("--parents")
     args.append("--set-depth")
     args.append(SVN_DEPTH_INFINITY)
@@ -41,6 +48,8 @@ def _parse_args():
     parser.add_argument("destination", help="The destination")
     parser.add_argument("input", help="The input file")
     parser.add_argument('--username', default=None, help="svn username")
+    parser.add_argument('--ask-password', action='store_true',
+                        help="ask for svn password")
     return parser.parse_args()
 
 def _parse_line(line, destination):
@@ -49,12 +58,12 @@ def _parse_line(line, destination):
 def on_complete(path):
   print(path)
 
-def checkout(url, destination, file, username):
+def checkout(url, destination, file, username, password):
     if not isdir(destination):
-        _checkout(url, destination, username)
+        _checkout(url, destination, username, password)
     for line in file:
         path = _parse_line(line, destination)
-        _update(path, username)
+        _update(path, username, password)
         on_complete(path)
 
 def main():
@@ -64,9 +73,13 @@ def main():
     destination = parser.destination
     input_path = parser.input
     username = parser.username
+    ask_password = parser.ask_password
+    password = None
+    if ask_password:
+        password = getpass('Password: ')
 
     with open(input_path, 'r') as file:
-      checkout(url, destination, file, username)
+      checkout(url, destination, file, username, password)
 
 if __name__ == "__main__":
     main()
