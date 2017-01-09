7db71808faa7d64511a577f35d4f0ece8b4f8aca
Emanuel Couto <unit73e@gmail.com>
2017-01-09 13:12:56 +0000

    Add username option

--- checkout.py
+++ checkout.py
@@ -3,25 +3,36 @@ import argparse
 from os.path import isdir
 
 SVN_COMMAND = "svn"
-SVN_DETPH_EMPTY = "empty"
-SVN_DETPH_INFINITY = "infinity"
+SVN_DEPTH_EMPTY = "empty"
+SVN_DEPTH_INFINITY = "infinity"
 
-def _checkout(url, path):
-    return subprocess.run([SVN_COMMAND, "co",
-                           "--depth", SVN_DETPH_EMPTY,
-                           "--quiet",
-                           "--non-interactive",
-                           url, path],
-                          check=True)
+def _checkout(url, path, username):
+    args = [SVN_COMMAND]
+    args.append("co")
+    if username:
+        args.append("--username")
+        args.append(username)
+    args.append("--depth")
+    args.append(SVN_DEPTH_EMPTY)
+    args.append("--quiet")
+    args.append("--non-interactive")
+    args.append(url)
+    args.append(path)
+    return subprocess.run(args, check=True)
 
-def _update(path):
-    return subprocess.run([SVN_COMMAND, "up",
-                          "--parents",
-                          "--set-depth", SVN_DETPH_INFINITY,
-                          "--quiet",
-                          "--non-interactive",
-                          path],
-                          check=True)
+def _update(path, username):
+    args = [SVN_COMMAND]
+    args.append("up")
+    if username:
+        args.append("--username")
+        args.append(username)
+    args.append("--parents")
+    args.append("--set-depth")
+    args.append(SVN_DEPTH_INFINITY)
+    args.append("--quiet")
+    args.append("--non-interactive")
+    args.append(path)
+    return subprocess.run(args, check=True)
 
 def _parse_args():
     parser = argparse.ArgumentParser(
@@ -29,6 +40,7 @@ def _parse_args():
     parser.add_argument("url", help="The root url")
     parser.add_argument("destination", help="The destination")
     parser.add_argument("input", help="The input file")
+    parser.add_argument('--username', default=None, help="svn username")
     return parser.parse_args()
 
 def _parse_line(line, destination):
@@ -37,12 +49,12 @@ def _parse_line(line, destination):
 def on_complete(path):
   print(path)
 
-def checkout(url, destination, file):
+def checkout(url, destination, file, username):
     if not isdir(destination):
-        _checkout(url, destination)
+        _checkout(url, destination, username)
     for line in file:
         path = _parse_line(line, destination)
-        _update(path)
+        _update(path, username)
         on_complete(path)
 
 def main():
@@ -51,9 +63,10 @@ def main():
     url = parser.url
     destination = parser.destination
     input_path = parser.input
+    username = parser.username
 
     with open(input_path, 'r') as file:
-      checkout(url, destination, file)
+      checkout(url, destination, file, username)
 
 if __name__ == "__main__":
     main()
